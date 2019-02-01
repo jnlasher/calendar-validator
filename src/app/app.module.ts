87cef8c0426a1dc4319b7eb65a955708a7442eea
Joel Lasher <jnlasher@ncsu.edu>
2019-02-01 14:26:25 -0500

    UI updates on the entire app. Cleaned out Bootstrap, added Angular-Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { FullMaterialModule } from './material-module';
 import { GoogleCalendarComponent } from './google-calendar/google-calendar.component';
 import { OfficeThreesixfiveComponent } from './office-threesixfive/office-threesixfive.component';
 import { MsExchangeComponent } from './ms-exchange/ms-exchange.component';
@@ -21,8 +24,11 @@ import { AppRoutingModule }     from './app-routing.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FullMaterialModule,
     FormsModule,
     HttpClientModule,
+    FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
